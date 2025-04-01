0adb40c6eb6154867b7b0833101e8e4fc1cb79c4
“alerenna” <alessandrorennafm@gmail.com>
2025-04-01 21:38:19 +0800

    Edit anchor tags with NavLink components in Header and Footer for improved routing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
             <footer className="p-5 d-flex bg-warning">
 
 
-                <a className="fs-3 text-decoration-none" alt="Logo" style={{ height: '40px', marginRight: '20px', color: 'black' }}>Giallo Booleano</a>
+                <NavLink to="/" className="fs-3 text-decoration-none" alt="Logo" style={{ height: '40px', marginRight: '20px', color: 'black' }}>Giallo Booleano</NavLink>
                 <div style={{ display: 'flex', gap: '20px' }}>
                     <nav>
                         <ul style={{ listStyle: 'none', padding: 0, margin: 0, display: 'flex', flexDirection: 'column', gap: '10px' }}>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
             <header>
                 <nav className="navbar navbar-expand-sm navbar-light bg-warning">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="#">Giallo Booleano</a>
+                        <NavLink className="navbar-brand" to="/">Giallo Booleano</NavLink>
                         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarID"
                             aria-controls="navbarID" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
